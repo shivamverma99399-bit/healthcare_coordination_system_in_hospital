904d7b9b1f6c8b22d7a90547aff75c3502b8d83a
Yuvraj VIjay Verma <shivamverma99399@gmail.com>
2026-04-04 18:05:37 +0530

    update

--- healthcare_system/build.sh
+++ healthcare_system/build.sh
@@ -2,4 +2,9 @@
 set -o errexit
 
 pip install -r requirements.txt
+
 python manage.py collectstatic --noinput --skip-checks
+
+python manage.py migrate --noinput
+
+python manage.py createsuperuser --noinput || true
