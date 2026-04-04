7070fabce94a30bbc89a74e97d216f8c096f4ba2
Yuvraj VIjay Verma <shivamverma99399@gmail.com>
2026-04-04 19:02:25 +0530

    update

--- healthcare_system/build.sh
+++ healthcare_system/build.sh
@@ -4,7 +4,11 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput --skip-checks
-
 python manage.py migrate --noinput
 
 python manage.py createsuperuser --noinput || true
+
+# 👇 ADD THESE
+python manage.py seed_data
+python manage.py seed_demo_access
+python manage.py seed_demo_data
