63bec7ccd5e2cd76636cfa56f65ed86c7d500493
Yuvraj VIjay Verma <shivamverma99399@gmail.com>
2026-04-04 17:06:14 +0530

    update

--- healthcare_system/core/apps.py
+++ healthcare_system/core/apps.py
@@ -1,5 +1,14 @@
 from django.apps import AppConfig
 
-
 class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@gmail.com",
+                password="admin123"
+            )
--- healthcare_system/healthcare_system/settings.py
+++ healthcare_system/healthcare_system/settings.py
@@ -99,7 +99,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'core',
+    'core.apps.CoreConfig',
 ]
 
 
